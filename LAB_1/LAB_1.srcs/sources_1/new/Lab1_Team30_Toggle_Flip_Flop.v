9ea22021f9afb4fc82e41792887783e1ee35ea9c
SamTech37 <SamTech37@users.noreply.github.com>
2024-09-12 19:02:08 +0800

    add: XOR implementation without primitive xor()

--- LAB_1/LAB_1.srcs/sources_1/new/Lab1_Team30_Toggle_Flip_Flop.v
+++ LAB_1/LAB_1.srcs/sources_1/new/Lab1_Team30_Toggle_Flip_Flop.v
@@ -8,3 +8,20 @@ output q;
 
 endmodule
 
+
+//gate-level stuff only
+module MyXOR(a, b, f);
+
+input a, b;
+output f;
+
+wire na,nb;
+wire t1,t2;
+
+not(na,a);
+not(nb,b);
+and(t1, a, nb);
+and(t2, na, b);
+or(f, t1, t2);
+
+endmodule;
