963b0b1f36702a363582493ce3ed9315cf940fdc
SamTech37 <SamTech37@users.noreply.github.com>
2024-09-12 19:38:51 +0800

    chore: Add D Flip Flop testbench and module

--- LAB_1/LAB_1.srcs/sources_1/new/Lab1_Team30_Toggle_Flip_Flop.v
+++ LAB_1/LAB_1.srcs/sources_1/new/Lab1_Team30_Toggle_Flip_Flop.v
@@ -6,6 +6,16 @@ input t;
 input rst_n;
 output q;
 
+wire q0,t1,t2;
+wire nq0;
+
+MyXOR exor(t1, q0, t);
+and(t2, t1, rst_n);
+D_Flip_Flop dff(clk,t2,q0);
+
+not(nq0,q0);
+not(q,nq0);
+
 endmodule
 
 
@@ -25,3 +35,35 @@ and(t2, na, b);
 or(f, t1, t2);
 
 endmodule;
+
+// DFF with 2 D-latches, tested
+module D_Flip_Flop(clk, d, q);
+input clk;
+input d;
+output q;
+
+wire nclk;
+wire temp;
+
+not(nclk,clk);
+D_Latch mas(nclk,d,temp);
+D_Latch sla(clk,temp,q);
+
+endmodule
+
+module D_Latch(e, d, q);
+input e;
+input d;
+output q;
+
+wire nd, q0, nq0;
+wire t1, t2;
+
+not(nd,d);
+nand(t1,d,e);
+nand(t2,nd,e);
+nand(q0, t1, nq0);
+nand(nq0, t2, q0);
+not(q, nq0);
+
+endmodule
