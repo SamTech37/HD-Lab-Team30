e6e7c85faaba7e9727e5eff3553c2482d82bc2ff
SamTech37 <SamTech37@users.noreply.github.com>
2024-09-17 17:59:19 +0800

    add fpga crossbar impl.

--- LAB_1/LAB_1.srcs/sources_1/new/Lab1_Team30_Crossbar_2x2_4bit_fpga.v
+++ LAB_1/LAB_1.srcs/sources_1/new/Lab1_Team30_Crossbar_2x2_4bit_fpga.v
@@ -20,11 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Crossbar_2x2_4bit_fpga(in1,in2,control,out1,out2);
+module Crossbar_2x2_4bit_fpga(in1,in2,control,out1_a,out1_b,out2_a,out2_b);
 input [4-1:0] in1, in2;
 input control;
-output [4-1:0] out1, out2;
+output [4-1:0] out1_a, out1_b;
+output [4-1:0] out2_a, out2_b;
 
+wire [4-1:0] out1,out2;
 
 Crossbar_2x2_4bit simple_cross(
     .in1(in1),
@@ -34,5 +36,30 @@ Crossbar_2x2_4bit simple_cross(
     .out2(out2)
 );
 
+fanout_1x2_4bit f1(out1, out1_a,out1_b);
+fanout_1x2_4bit f2(out2, out2_a,out2_b);
+
+
+endmodule
+
+module fanout_1x2_4bit(in,out1,out2);
+input [4-1:0] in;
+output [4-1:0] out1,out2;
+
+wire [4-1:0] t;
+not(t[0],in[0]);
+not(t[1],in[1]);
+not(t[2],in[2]);
+not(t[3],in[3]);
+
+not(out1[0],t[0]);
+not(out1[1],t[1]);
+not(out1[2],t[2]);
+not(out1[3],t[3]);
+
+not(out2[0],t[0]);
+not(out2[1],t[1]);
+not(out2[2],t[2]);
+not(out2[3],t[3]);
 
 endmodule
