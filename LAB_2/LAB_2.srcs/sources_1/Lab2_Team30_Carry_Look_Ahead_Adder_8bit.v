86666135121d07880b0e2342ae67cb4929e99b1a
SamuelHou <yoyusamuel@gmail.com>
2024-09-24 21:35:10 +0800

    Add CLA

--- LAB_2/LAB_2.srcs/sources_1/Lab2_Team30_Carry_Look_Ahead_Adder_8bit.v
+++ LAB_2/LAB_2.srcs/sources_1/Lab2_Team30_Carry_Look_Ahead_Adder_8bit.v
@@ -1,4 +1,134 @@
 `timescale 1ns/1ps
+`timescale 1ns/1ps
+
+module NOT(out, a);
+input a;
+output out;
+nand NAND1(out, a ,a);
+endmodule 
+
+module AND(out, a, b);
+input a, b;
+output out;
+wire temp;
+nand NAND1(temp, a, b);
+NOT N1(out, temp);
+endmodule
+
+module OR(out, a, b);
+input a, b;
+output out;
+wire abar, bbar;
+NOT N1(abar, a);
+NOT N2(bbar, b);
+nand NAND1(out, abar, bbar);
+endmodule
+
+module NOR(out, a, b);
+input a, b;
+output out;
+wire temp;
+OR O1(temp, a, b);
+NOT N1(out, temp);
+endmodule
+
+module XOR(out, a, b);
+input a, b;
+output out;
+wire abar, bbar, temp1, temp2;
+NOT N1(abar, a);
+NOT N2(bbar, b);
+AND A1(temp1, a, bbar);
+AND A2(temp2, b, abar);
+OR O1(out, temp1, temp2);
+endmodule
+
+module XNOR(out, a, b);
+input a, b;
+output out;
+wire abar, bbar, temp1, temp2;
+NOT N1(abar, a);
+NOT N2(bbar, b);
+AND A1(temp1, a, b);
+AND A2(temp2, bbar, abar);
+OR O1(out, temp1, temp2);
+endmodule
+
+
+module Half_Adder(a, b, cout, sum);
+input a, b;
+output cout, sum;
+XOR XO1(sum, a, b);
+AND A1(cout, a, b);
+endmodule
+
+module Full_Adder (a, b, cin, sum);
+input a, b, cin;
+output sum;
+wire tempc, temps, tc;
+Half_Adder H1(a, b, tempc, temps);
+Half_Adder H2(cin, temps, tc, sum);
+endmodule
+
+module PG_Produce(a, b, p, g);
+input [3:0] a, b;
+output [3:0] p, g;
+AND A1(g[0], a[0], b[0]);
+OR O1(p[0], a[0], b[0]);
+AND A1(g[1], a[1], b[1]);
+OR O1(p[1], a[1], b[1]);
+AND A1(g[2], a[2], b[2]);
+OR O1(p[2], a[2], b[2]);
+AND A1(g[3], a[3], b[3]);
+OR O1(p[3], a[3], b[3]);
+endmodule
+
+//4bit CLA
+module Carry_Look_Ahead_Gen_4bit(c, a, b, g, p, s);
+input c;
+input [3:0] a, b;
+output [3:0] g, p, s;
+wire [3:0] g, p;
+wire [4:1] cout;
+PG_Produce PP1(a, b, p, g);
+wire t0, t1, t2, t3;
+//Produce C1
+AND A1(t0, p[0], c);
+OR O1(c[1], g[0], t1);
+//Produce C2
+AND A2(t1, p[1], c[1]);
+OR O2(c[2], g[1], t1);
+//Produce C3
+AND A2(t2, p[2], c[2]);
+OR O2(c[3], g[2], t2);
+//Produce [3:0]sum
+Full_Adder FA1(a[0], b[0], c, s[0]);
+Full_Adder FA2(a[1], b[1], c[1], s[1]);
+Full_Adder FA3(a[2], b[2], c[2], s[2]);
+Full_Adder FA4(a[3], b[3], c[3], s[3]);
+endmodule
+
+module Carry_Look_Ahead_Gen_2bit(cin, g, p, cout);
+input cin;
+input [3:0] g, p;
+output cout;
+wire pg, gg, t1, t2, t3, t4, t5, t6, t7, o1, o2;
+//pg
+AND A1(t1, p[0], p[1]);//p0p1
+AND A2(t2, p[2], p[3]);//p2p3
+AND A3(pg, t1, t2);
+//gg
+AND A4(t3, g[2], p[3]);//g2p3
+AND A6(t4, t2, g[1]);//g1p3p2
+AND A7(t5, g[0], p[1]);//g0p1
+AND A8(t6, t2, t5);//g0p3p2p1
+OR O1(o1, g[3], t3);
+OR O2(o2, t4, t6);
+OR O3(gg, o1, o2);
+//cg
+AND A1(t7, pg, cin);
+OR O4(cout, t7, gg);
+endmodule
 
 //8bit CLA
 module Carry_Look_Ahead_Adder_8bit(a, b, c0, s, c8);
@@ -6,5 +136,10 @@ input [8-1:0] a, b;
 input c0;
 output [8-1:0] s;
 output c8;
-
+wire [3:0] g1, g2, p1, p2;
+wire c4;
+Carry_Look_Ahead_Gen_4bit CLA4_1(c0, a[3:0], b[3:0], g1, p1, s[3:0]);
+Carry_Look_Ahead_Gen_2bit CLA2_1(c0, g1, p1, c4);
+Carry_Look_Ahead_Gen_4bit CLA4_2(c4, a[7:4], b[7:4], g2, p2, s[7:4]);
+Carry_Look_Ahead_Gen_2bit CLA2_1(c4, g2, p2, c8);
 endmodule
--- LAB_2/LAB_2.srcs/sources_1/Lab2_Team30_Decode_And_Execute.v
+++ LAB_2/LAB_2.srcs/sources_1/Lab2_Team30_Decode_And_Execute.v
@@ -17,6 +17,7 @@ endmodule
 
 module NAND(out, a, b);
 input a, b;
+output out;
 wire temp;
 AND A1(temp, a, b);
 NOT N1(out, temp);
@@ -61,12 +62,186 @@ AND A2(t2, a, b);
 OR O1(out, t1, t2);
 endmodule
 
-module Half_Substracter(a, b, bi, bi1);
-//a-b, bi means lend to the next bit, bi1 means borrow from the previous bit
+/*
+module Half_Substracter(a, b, d, bi1);
+//a-b, bi1 means borrow from the previous bit, d means the difference between a and b
 input a, b;
-output bi, bi1;
+output d, bi1;
+wire abar;
+NOT N1(abar, a);
+XOR X1(d, a, b);
+AND A1(bi1, abar, b);
+endmodule
 
+module Full_Substracter(a, b, bi, d, bi1);
+//a-b, bi means lend to the next bit, bi1 means borrow from the previous bit, d means the difference between a and b
+input a, b, bi;
+output d, bi1;
+wire tempbi1, tempd, tbi1;
+Half_Substracter HS1(a, b, tempd, tempbi1);
+Half_Substracter HS2(tempd, bi, d, tbi1);
+OR O1(bi1, tempbi1, tbi1);
+endmodule
 
+//4-bit RBS
+module Ripple_Borrow_Subtracter(rs, rt, rd);
+input [3:0] rs, rt;
+output [3:0] rd;
+wire [3:0] b;
+
+Full_Subtracter fs0(rs[0], rt[0], 0, rd[0], b[0]);
+Full_Subtracter fs1(rs[1], rt[1], b[0], rd[1], b[1]);
+Full_Subtracter fs2(rs[2], rt[2], b[1], rd[2], b[2]);
+Full_Subtracter fs3(rs[3], rt[3], b[2], rd[3], b[3]);
+endmodule*/
+
+module Ripple_Borrow_Subtracter(rs, rt, rd);
+input [3:0] rs;
+input [3:0] rt;
+output [3:0] rd;
+wire [3:0] temprt, trt;
+//2's complement
+NOT N1(temprt[0], rt[0]);
+NOT N2(temprt[1], rt[1]);
+NOT N3(temprt[2], rt[2]);
+NOT N4(temprt[3], rt[3]);
+Ripple_Carry_Adder RCA1(temprt, 4'b0001, trt);
+Ripple_Carry_Adder RCA2(rs, trt, rd);
+endmodule
+
+module Half_Adder(a, b, cout, sum);
+input a, b;
+output cout, sum;
+AND a1(cout,a,b);
+XOR ex(sum,a,b);
+endmodule
+
+module Full_Adder (a, b, cin, cout, sum);
+input a, b, cin;
+output cout, sum;
+wire s0;
+Majority maj(a,b,cin,cout);
+Half_Adder ha1(.a(a), .b(b), .sum(s0));
+Half_Adder ha2(.a(s0), .b(cin), .sum(sum));
+endmodule
+
+module Majority(a, b, c, out);
+input a, b, c;
+output out;
+wire t0,t1,t2,t3;
+AND a1(t0,a,b);
+AND a2(t1,b,c);
+AND a3(t2,c,a);
+XOR ex1(t3,t0,t1);
+XOR ex2(out,t2,t3);
+endmodule
+
+module Ripple_Carry_Adder(a, b, sum);
+input [3:0] a, b;
+output [3:0] sum;
+wire [3:0] c;
+Full_Adder fa0(a[0],b[0],0,c[0],sum[0]);
+Full_Adder fa1(a[1],b[1],c[0],c[1],sum[1]);
+Full_Adder fa2(a[2],b[2],c[1],c[2],sum[2]);
+Full_Adder fa3(a[3],b[3],c[2],c[3],sum[3]);
+endmodule
+
+module Bit_Wise_OR(rs, rt, rd);
+input [3:0] rs;
+input [3:0] rt;
+output [3:0] rd;
+OR O1(rd[0], rs[0], rt[0]);
+OR O2(rd[1], rs[1], rt[1]);
+OR O3(rd[2], rs[2], rt[2]);
+OR O4(rd[3], rs[3], rt[3]);
+endmodule
+
+module Bit_Wise_AND(rs, rt, rd);
+input [3:0] rs;
+input [3:0] rt;
+output [3:0] rd;
+AND A1(rd[0], rs[0], rt[0]);
+AND A2(rd[1], rs[1], rt[1]);
+AND A3(rd[2], rs[2], rt[2]);
+AND A4(rd[3], rs[3], rt[3]);
+endmodule
+
+module Right_Shift(rt, rd);
+input [3:0] rt;
+output [3:0] rd;
+AND A1(rd[0], rt[1], 1);
+AND A2(rd[1], rt[2], 1);
+AND A3(rd[2], rt[3], 1);
+AND A4(rd[3], rt[0], 1);
+endmodule
+
+module Left_Shift(rs, rd);
+input [3:0] rs;
+output [3:0] rd;
+AND A1(rd[0], rs[3], 1);
+AND A2(rd[1], rs[0], 1);
+AND A3(rd[2], rs[1], 1);
+AND A4(rd[3], rs[2], 1);
+endmodule
+
+module COMPARE_LT(rs, rt, rd);
+//(rs < rt) = (rs[3])'(rt[3]) + (x[3])(rs[2])'(rt[2]) + (x[3])(x[2])(rs[1])'(rt[1]) + (x[3])(x[2])(x[1])(rs[0])'(rt[0])
+input [3:0] rs, rt;
+output [3:0] rd;
+wire [3:0] x;
+wire rs0b, rs1b, rs2b, rs3b, ans3, ans2, ans1, ans0;
+NOT N1(rs0b, rs[0]);
+NOT N2(rs1b, rs[1]);
+NOT N3(rs2b, rs[2]);
+NOT N4(rs3b, rs[3]);
+X_Produce XP1(x[3], rs[3], rt[3]);
+X_Produce XP2(x[2], rs[2], rt[2]);
+X_Produce XP3(x[1], rs[1], rt[1]);
+wire t0, t1, t2, t3, t4, t5, o1, o2, f1;
+AND A1(t3, rs3b, rt[3]);
+AND A2(ans2, rs2b, rt[2]);
+AND A3(ans1, rs1b, rt[1]);
+AND A4(ans0, rs0b, rt[0]);
+AND A5(t2, ans2, x[3]);
+AND A6(t4, x[3], x[2]);
+AND A7(t1, ans1, t4);
+AND A8(t5, t4, x[1]);
+AND A9(t0, ans0, t5);
+OR O1(o1, t0, t1);
+OR O2(o2, t2, t3);
+OR O3(f1, o1, o2);
+AND A10(rd[3], 1, 1);
+AND A11(rd[2], 1, 0);
+AND A12(rd[1], 1, 1);
+AND A13(rd[0], 1, f1);
+endmodule
+
+module COMPARE_EQ(rs, rt, rd);
+input [3:0] rs, rt;
+output [3:0] rd;
+wire [3:0] x;
+wire t1, t2;
+AND A1(rd[3], 1, 1);
+AND A2(rd[2], 1, 1);
+AND A3(rd[1], 1, 1);
+X_Produce XP0(x[0], rs[0], rt[0]);
+X_Produce XP1(x[1], rs[1], rt[1]);
+X_Produce XP2(x[2], rs[2], rt[2]);
+X_Produce XP3(x[3], rs[3], rt[3]);
+AND A4(t1, x[0], x[1]);
+AND A5(t2, x[2], x[3]);
+AND A6(rd[0], t1, t2);
+endmodule
+
+module X_Produce(x, a, b);
+input a, b;
+output x;
+wire abar, bbar, t1, t2;
+NOT N1(abar, a);
+NOT N2(bbar, b);
+OR O1(t1, a, b);
+OR O2(t2, abar, bbar);
+AND A1(x, t1, t2);
 endmodule
 
 module MUX_2x1(a,b,sel,out);
@@ -97,5 +272,16 @@ module Decode_And_Execute(rs, rt, sel, rd);
 input [4-1:0] rs, rt;
 input [3-1:0] sel;
 output [4-1:0] rd;
-
+wire [3:0] t0, t1, t2, t3, t4, t5, t6, t7, s1, s2;
+Ripple_Borrow_Subtracter RBS1(rs, rt, t0);
+Ripple_Carry_Adder RCA1(rs, rt, t1);
+Bit_Wise_OR BWR1(rs, rt, t2);
+Bit_Wise_AND BWA1(rs, rt, t3);
+Right_Shift RS1(rt, t4);
+Left_Shift LS1(rs, t5);
+COMPARE_LT CL1(rs, rt, t6);
+COMPARE_EQ CE1(rs, rt, t7);
+MUX_4x1 m1(t0,t1,t2,t3, sel[1:0],s1);
+MUX_4x1 m2(t4,t5,t6,t7, sel[1:0],s2);
+MUX_2x1 m3(s1,s2,sel[2], rd);
 endmodule
