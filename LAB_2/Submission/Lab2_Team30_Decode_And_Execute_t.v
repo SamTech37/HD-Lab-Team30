273150ebd50baa6b566e2f8c3c0fc86fb0df2cf2
SamTech37 <SamTech37@users.noreply.github.com>
2024-09-26 21:30:17 +0800

    add sources for submission, tested on CAD server

---
+++ LAB_2/Submission/Lab2_Team30_Decode_And_Execute_t.v
@@ -0,0 +1,32 @@
+`timescale 1ns/1ps
+
+
+module Decode_And_Execute_T();
+// I/O signals
+reg [4-1:0] rs, rt ;
+reg [3-1:0] sel;
+wire [4-1:0] rd;
+
+
+Decode_And_Execute DnE(
+    .rs(rs), .rt(rt), .sel(sel), .rd(rd)  
+);
+
+initial begin
+    rs = 4'b0000;
+    rt = 4'b0000;
+    sel = 3'b000;
+    repeat (4) begin
+        
+        rs = rs + 3;
+        rt = rt + 5;
+        
+        repeat (2**3) begin
+            #1 sel = sel + 1;
+        end
+    end
+    #1 $finish;
+
+end
+
+endmodule
