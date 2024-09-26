98167af2094a5e75da341c5edcaa6391d0660ee8
SamTech37 <SamTech37@users.noreply.github.com>
2024-09-26 19:56:56 +0800

    update RCA testbench

--- LAB_2/LAB_2.srcs/sources_1/new/Lab2_Team30_Ripple_Carry_Adder_t.v
+++ LAB_2/LAB_2.srcs/sources_1/new/Lab2_Team30_Ripple_Carry_Adder_t.v
@@ -1,13 +1,10 @@
 `timescale 1ns/1ps
-
-//testbench for 8-bit RCA
 module Ripple_Carry_Adder_T();
 // I/O signals
 reg [8-1:0] a = 8'b0000_0000, b=8'b0000_0000;
 reg cin = 1'b0;
 wire [8-1:0] sum;
 wire cout;
-
 Ripple_Carry_Adder RCA(
     .a(a),
     .b(b),
@@ -15,13 +12,17 @@ Ripple_Carry_Adder RCA(
     .cout(cout),
     .sum(sum)
 );
-
+reg error = 1'b0;
 initial begin
-    repeat (2**3) begin
-        #1 cin=~cin;
-        a = (a << 2) + 1;//right shift 2-bits then add 1
-        b = (b << 1) + 1;
+    //make sure the module works under all cases, bitwise.
+    repeat (2**17) begin
+        #1
+        error = !( a+b+cin === {cout,sum});
+        #1
+        //next input pattern
+        {a,b,cin} = {a,b,cin}+ 1'b1;
     end
+    
     #1 $finish;
 end
 
