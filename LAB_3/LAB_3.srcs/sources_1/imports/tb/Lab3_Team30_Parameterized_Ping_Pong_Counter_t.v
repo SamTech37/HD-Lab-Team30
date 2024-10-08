dfcc34c8c6b856b73ab22bd07a05322948690f13
SamTech37 <SamTech37@users.noreply.github.com>
2024-10-08 20:33:05 +0800

    revised ping pong counter design, add code & tb for Q5

--- LAB_3/LAB_3.srcs/sources_1/imports/tb/Lab3_Team30_Parameterized_Ping_Pong_Counter_t.v
+++ LAB_3/LAB_3.srcs/sources_1/imports/tb/Lab3_Team30_Parameterized_Ping_Pong_Counter_t.v
@@ -6,10 +6,12 @@ module Parameterized_Ping_Pong_Counter_T();
 reg clk = 1'b1, rst_n =1'b1;
 reg enable = 1'b1;
 reg flip = 1'b0;
-reg [4-1:0] max = 4'b1001;
-reg [4-1:0] min = 4'b0011;
+reg [4-1:0] max;
+reg [4-1:0] min;
 wire direction;
 wire [4-1:0] out;
+
+
 
 //test instance
 Parameterized_Ping_Pong_Counter ppp_counter (
@@ -28,28 +30,53 @@ Parameterized_Ping_Pong_Counter ppp_counter (
 parameter cyc = 10;
 always#(cyc/2)clk = !clk;
 
-//exhaustive test with error raising
-reg error = 1'b0;;
 
-
-//need more rigorous testing
+//[TODO]                                
+//handle enable                         
+//handle max & min & out-of-range       
+//handle flip                           
 
 initial begin
     //initialize inputs
     @ (negedge clk)
     rst_n = 1'b0;
     enable = 1'b1;
+    min = 4'd2;
+    max = 4'd10;
     @ (negedge clk)
 	rst_n = 1'b1;
 	
+	//counting up & down
+	#(cyc*16)
+	max = 4'd9;
+	#(cyc*4)
+	min = 4'd3;
+	
+	#(cyc*16)
+	//test disable
+	enable = 1'b0;
+	#(cyc*4) enable = 1'b1;
+	
+	//test flip
+    @ (negedge clk)
+	#(cyc) flip = 1'b1;
+	#(cyc) flip = 1'b0;
 	@ (negedge clk)
-	repeat(2**2) begin
-    #(cyc * 12) enable = 1'b0;
-    #(cyc * 3) enable = 1'b1;
-	end
+	#(cyc*4) flip = 1'b1;
+	#(cyc*4) flip = 1'b0;
+	
+	//test out of range
+    @ (negedge clk)
+    max = 4'd5;
+    min = 4'd9;
+    #(cyc*4)
+    
     
     #(cyc) $finish;
 end
+
+//comb
+
 
 
 endmodule
