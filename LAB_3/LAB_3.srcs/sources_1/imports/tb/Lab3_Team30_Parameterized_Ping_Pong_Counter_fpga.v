50b7e2d84809766ee12d46cb6ebbda8c399504d8
SamTech37 <SamTech37@users.noreply.github.com>
2024-10-09 14:48:59 +0800

    add onepulse & debounce submodule

--- LAB_3/LAB_3.srcs/sources_1/imports/tb/Lab3_Team30_Parameterized_Ping_Pong_Counter_fpga.v
+++ LAB_3/LAB_3.srcs/sources_1/imports/tb/Lab3_Team30_Parameterized_Ping_Pong_Counter_fpga.v
@@ -13,7 +13,7 @@ output reg [7-1:0] outLED;//7-seg
 
 
 reg [7-1:0] dirLED;//7-seg display 1 & 0
-reg [7-1:0] outLED1, outLED2;//7-seg display 3 & 2
+reg [7-1:0] cntLED1, cntLED2;//7-seg display 3 & 2
 
 wire rst_n_in, flip_in;//debounce & one-pulse for button input
 wire direction;
@@ -37,23 +37,23 @@ Parameterized_Ping_Pong_Counter ppp_counter (
 //7-seg display output 
 always @(posedge clk) begin
     case(out)  //need to change
-    4'b0000: outLED1 <= 7'b0000001; // "0"     
-    4'b0001: outLED1 <= 7'b1001111; // "1" 
-    4'b0010: outLED1 <= 7'b0010010; // "2" 
-    4'b0011: outLED1 <= 7'b0000110; // "3" 
-    4'b0100: outLED1 <= 7'b1001100; // "4" 
-    4'b0101: outLED1 <= 7'b0100100; // "5" 
-    4'b0110: outLED1 <= 7'b0100000; // "6" 
-    4'b0111: outLED1 <= 7'b0001111; // "7" 
-    4'b1000: outLED1 <= 7'b0000000; // "8"     
-    4'b1001: outLED1 <= 7'b0000100; // "9" 
-    4'b1010: outLED1 <= 7'b0001000; // "A" 
-    4'b1011: outLED1 <= 7'b1100000; // "b" 
-    4'b1100: outLED1 <= 7'b1110010; // "c" 
-    4'b1101: outLED1 <= 7'b1000010; // "d" 
-    4'b1110: outLED1 <= 7'b0110000; // "E" 
-    4'b1111: outLED1 <= 7'b0111000; // "F" 
-    default: outLED1 <= 7'b0000001; // "0"
+    4'b0000: cntLED1 <= 7'b0000001; // "0"     
+    4'b0001: cntLED1 <= 7'b1001111; // "1" 
+    4'b0010: cntLED1 <= 7'b0010010; // "2" 
+    4'b0011: cntLED1 <= 7'b0000110; // "3" 
+    4'b0100: cntLED1 <= 7'b1001100; // "4" 
+    4'b0101: cntLED1 <= 7'b0100100; // "5" 
+    4'b0110: cntLED1 <= 7'b0100000; // "6" 
+    4'b0111: cntLED1 <= 7'b0001111; // "7" 
+    4'b1000: cntLED1 <= 7'b0000000; // "8"     
+    4'b1001: cntLED1 <= 7'b0000100; // "9" 
+    4'b1010: cntLED1 <= 7'b0001000; // "A" 
+    4'b1011: cntLED1 <= 7'b1100000; // "b" 
+    4'b1100: cntLED1 <= 7'b1110010; // "c" 
+    4'b1101: cntLED1 <= 7'b1000010; // "d" 
+    4'b1110: cntLED1 <= 7'b0110000; // "E" 
+    4'b1111: cntLED1 <= 7'b0111000; // "F" 
+    default: cntLED1 <= 7'b0000001; // "0"
     endcase
 
     case(direction)
@@ -68,18 +68,17 @@ reg [2-1:0]display_digit;
 always @(posedge clk_2_17) begin
     case (display_digit) 
     2'b00: begin
-        outLED = outLED1;
+        outLED = cntLED1;
         an = 4'b0111;
         display_digit <= 2'b01;
     end
     2'b01: begin
-        outLED = outLED2;
+        outLED = cntLED2;
         an = 4'b1011;
         display_digit <= 2'b10;
     end
 
-    2'b10:
-    2'b11: begin
+    2'b10, 2'b11: begin
         outLED = dirLED;
         an = 4'b1100;
         display_digit <= 2'b00;
@@ -99,8 +98,32 @@ endmodule
 /*submodule used*/
 
 //debounce
+module Debounce(clk, pb_in, pb_debounced);
+input clk, pb_in;
+output pb_debounced;
+
+reg [4-1:0] DFF;
+
+always @(posedge clk) begin
+    DFF <= {DFF[2:0], pb_in};
+end
+assign pb_debounced = (DFF == 4'b11111)? 1'b1 : 1'b0;
+
+endmodule
 
 //one-pulse
+module One_Pulse(clk,  pb_debounced, pb_onepulse);
+
+input clk, pb_debounced;
+output reg pb_onepulse;
+reg pb_debounced_delay;
+
+always @(posedge clk) begin
+    pb_debounced_delay <= pb_debounced;
+    pb_onepulse <= (pb_debounced && !pb_debounced_delay)? 1'b1 : 1'b0;
+end
+
+endmodule
 
 //clock divider for time-multiplexing
 module Clock_Divider(clk, rst_n, clk_2_17);
