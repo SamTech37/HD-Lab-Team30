79f767645b7a0d22d7023d71aa3937380b088cc3
SamTech37 <SamTech37@users.noreply.github.com>
2024-10-14 12:54:24 +0800

    Refactor memory block declaration in tb/Lab4_Team30_Content_Addressable_Memory_t.v

--- LAB_4/LAB_4.srcs/sources_1/tb/Lab4_Team30_Content_Addressable_Memory_t.v
+++ LAB_4/LAB_4.srcs/sources_1/tb/Lab4_Team30_Content_Addressable_Memory_t.v
@@ -8,7 +8,6 @@ reg [3:0] addr;
 wire [3:0] dout;
 wire hit;
 
-reg [8-1:0] mem [16-1:0]; // a memory block of 16 lines * 8-bit 
 
 Content_Addressable_Memory CAM(clk, wen, ren, din, addr, dout, hit);
 
