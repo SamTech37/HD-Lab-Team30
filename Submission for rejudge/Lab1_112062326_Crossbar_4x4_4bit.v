d887d8312ca54a79b72a3002baff4ed310c2e38d
SamTech37 <SamTech37@users.noreply.github.com>
2024-10-14 23:25:57 +0800

    lab1 practice

---
+++ Submission for rejudge/Lab1_112062326_Crossbar_4x4_4bit.v
@@ -0,0 +1,87 @@
+`timescale 1ns/1ps
+
+module Crossbar_4x4_4bit(in1, in2, in3, in4, out1, out2, out3, out4, control);
+input [4-1:0] in1, in2, in3, in4;
+input [5-1:0] control;
+output [4-1:0] out1, out2, out3, out4;
+
+wire [4-1:0]t1,t2,t3,t4,t5,t6;
+
+Crossbar_2x2_4bit cb0(in1,in2,control[0],t1,t2);
+Crossbar_2x2_4bit cb3(in3,in4,control[3],t3,t4);
+Crossbar_2x2_4bit cb2(t2,t3,control[2],t5,t6);
+Crossbar_2x2_4bit cb1(t1,t5,control[1],out1,out2);
+Crossbar_2x2_4bit cb4(t6,t4,control[4],out3,out4);
+
+endmodule
+
+
+/*
+submodules used
+*/
+
+module Crossbar_2x2_4bit(in1, in2, control, out1, out2);
+input [4-1:0] in1, in2;
+input control;
+output [4-1:0] out1, out2;
+
+wire nControl;
+wire [4-1:0] t1,t2,t3,t4;
+
+not neg(nControl, control);
+
+Dmux_1x2_4bit d1(in1,t1,t2,control);
+Dmux_1x2_4bit d2(in2,t3,t4,nControl);
+
+Mux_1x2_4bit m1(t1,t3,out1,control);
+Mux_1x2_4bit m2(t2,t4,out2,nControl);
+
+endmodule
+
+
+module Mux_1x2_4bit(in1,in2,out,sel);
+input [4-1:0] in1,in2;
+input sel;
+output [4-1:0] out;
+
+wire nSel;
+wire [4-1:0] t1,t2;
+
+not neg(nSel,sel);
+
+and a0(t1[0],in1[0],nSel);
+and a1(t1[1],in1[1],nSel);
+and a2(t1[2],in1[2],nSel);
+and a3(t1[3],in1[3],nSel);
+
+and a4(t2[0],in2[0],sel);
+and a5(t2[1],in2[1],sel);
+and a6(t2[2],in2[2],sel);
+and a7(t2[3],in2[3],sel);
+
+or or0(out[0],t1[0],t2[0]);
+or or1(out[1],t1[1],t2[1]);
+or or2(out[2],t1[2],t2[2]);
+or or3(out[3],t1[3],t2[3]);
+endmodule
+
+
+module Dmux_1x2_4bit(in, out1, out2, sel);
+input [3:0] in;
+input sel;
+output [3:0] out1, out2;
+wire nsel;
+
+not neg(nsel,sel);
+
+and a0(out1[0],in[0],nsel);
+and a1(out1[1],in[1],nsel);
+and a2(out1[2],in[2],nsel);
+and a3(out1[3],in[3],nsel);
+
+and a4(out2[0],in[0],sel);
+and a5(out2[1],in[1],sel);
+and a6(out2[2],in[2],sel);
+and a7(out2[3],in[3],sel);
+endmodule
+
--- Submission for rejudge/Lab1_112062326_Toggle_Flip_Flop.v
+++ Submission for rejudge/Lab1_112062326_Toggle_Flip_Flop.v
@@ -9,6 +9,11 @@ input t;
 input rst_n;
 output q;
 
+wire t1,t2;
+
+MyXOR exor(q,t,t1);
+and a1(t2,t1,rst_n);
+D_Flip_Flop DFF(clk,t,q);
 
 
 endmodule
@@ -16,18 +21,31 @@ endmodule
 
 //gate-level stuff only
 module MyXOR(a, b, f);
-
 input a, b;
 output f;
+wire na,nb;
+wire t1,t2;
 
-
+not n1(na,a);
+not n2(nb,b);
+and a1(t1,na,b);
+and a2(t2,a,nb);
+or o1(f,t1,t2);
 endmodule
 
-// DFF with 2 D-latches, tested
+// DFF with 2 D-latches
+//tested
 module D_Flip_Flop(clk, d, q);
 input clk;
 input d;
 output q;
+
+wire temp;
+wire nclk;
+
+not n1(nclk,clk);
+D_Latch mas(nclk,d,temp);
+D_Latch sla(clk,temp,q);
 
 endmodule
 
@@ -36,5 +54,16 @@ input e;
 input d;
 output q;
 
+wire nd;
+wire t1,t2;
+wire out, nout;
+
+not n1(nd,d);
+nand na1(t1,d,e);
+nand na2(t2,nd,e);
+nand na3(out,t1,nout);
+nand na4(nout,t2,out);
+
+not n2(q,nout);
 
 endmodule
