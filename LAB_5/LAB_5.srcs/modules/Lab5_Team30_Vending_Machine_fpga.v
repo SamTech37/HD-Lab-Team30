60f3bdc345d508344f1c5e0439a6945565fa651d
SamTech37 <SamTech37@users.noreply.github.com>
2024-11-07 01:27:52 +0800

    vendor: add mechanism for change return

--- LAB_5/LAB_5.srcs/modules/Lab5_Team30_Vending_Machine_fpga.v
+++ LAB_5/LAB_5.srcs/modules/Lab5_Team30_Vending_Machine_fpga.v
@@ -2,7 +2,7 @@
 
 // [TODO]
 // add keyboard IO
-// add mechanism for change return
+
 
 module Vending_Machine_FPGA (
 inout wire PS2_DATA,//use keyboard A,S,D,F for selecting the item
@@ -18,11 +18,12 @@ output wire [3:0] digit,
 output wire [3:0] outLED //LD3~LD0
 );
 
+wire [3:0] drink_select;
+wire key_A, key_S, key_D, key_F;
 
 wire btnL_db,btnC_db,btnR_db,btnD_db,btnU_db;
 wire insert_five,insert_ten,insert_fifty;
-wire reset, clear;
-wire rst_n;
+wire rst, clear;
 //debounce & onepulse for btns
 Debounce db1(.clk(clk),.reset(rst),.pb(btnL),.pb_debounced(btnL_db));
 Debounce db2(.clk(clk),.reset(rst),.pb(btnC),.pb_debounced(btnC_db));
@@ -34,16 +35,26 @@ OnePulse op1(.clock(clk),.signal(btnL_db),.signal_single_pulse(insert_five));
 OnePulse op2(.clock(clk),.signal(btnC_db),.signal_single_pulse(insert_ten));
 OnePulse op3(.clock(clk),.signal(btnR_db),.signal_single_pulse(insert_fifty));
 OnePulse op4(.clock(clk),.signal(btnD_db),.signal_single_pulse(clear));
-OnePulse op5(.clock(clk),.signal(btnU_db),.signal_single_pulse(reset));
-assign rst_n = !reset;
+OnePulse op5(.clock(clk),.signal(btnU_db),.signal_single_pulse(rst));
 
 
 wire [8:0] cash;
-Vending_Machine_FSM vendor(clk, reset, clear, insert_five, insert_ten, insert_fifty, outLED, cash);
+Vending_Machine_FSM vendor(
+    .clk(clk),
+    .rst(rst),
+    .clear(clear),
+    .insert_five(insert_five),
+    .insert_ten(insert_ten),
+    .insert_fifty(insert_fifty),
+    .drink_select(4'b0000), //change to key signals later
+    .drink_avail(outLED),
+    .cash(cash));
+
 SevenSegmentDisplay ssd(.display(display),.digit(digit),.cash(cash),.rst(reset),.clk(clk));
 
 endmodule
 
+//Moore FSM
 module Vending_Machine_FSM (
     input clk,
     input rst,//active high reset, be careful
@@ -51,6 +62,7 @@ module Vending_Machine_FSM (
     input insert_five,
     input insert_ten,
     input insert_fifty,
+    input [3:0]drink_select,
     output [3:0]drink_avail,
     output reg [8:0]cash 
 );
@@ -59,6 +71,7 @@ module Vending_Machine_FSM (
 reg [8:0] next_cash;
 parameter MAX = 9'd100;
 parameter MIN = 9'd0;
+parameter CHANGE_UNIT = 9'd5;
 parameter WATER = 9'd20;
 parameter OOLONG = 9'd25;
 parameter COKE = 9'd30;
@@ -70,21 +83,72 @@ assign drink_avail[2] = (cash >= COKE);
 assign drink_avail[3] = (cash >= COFFEE);
 
 
+wire select;//return 1 if any drink is selected and available
+assign select = |(drink_select&drink_avail); //unary reduction operator
+
+
+
+reg [2-1:0] state, next_state;
+parameter IDLE = 2'b00;//waiting for coins
+parameter BUY = 2'b01;//dispensing drink
+parameter CHANGE = 2'b10;//returning change
+
+reg [26-1:0] clock_divider;// ~1Hz clock
+always @(posedge clk, posedge rst) begin
+    if (rst) begin
+        clock_divider <= 26'b0;
+    end else begin
+        clock_divider <= clock_divider + 26'b1;
+    end
+end
 
 
 //seq
 always @(posedge clk) begin 
 if(rst) begin
     cash <= MIN;
+    state <= IDLE;
 end
 else begin 
+    state <= next_state;
     cash <= (next_cash > MAX) ? MAX : next_cash;
+
+   case(state)
+       BUY: begin
+       end
+       CHANGE: begin
+           if(clock_divider == {26{1'b1}}) begin
+               cash <= cash - CHANGE_UNIT;
+           end else cash <= cash;
+       end
+       default: begin //IDLE
+           cash <= (next_cash > MAX) ? MAX : next_cash;
+       end
+   endcase 
 end
 end
 
 //comb
 always @(*) begin
-   next_cash = cash + insert_five*5 + insert_ten*10 + insert_fifty*50;
+    next_cash = cash + insert_five*5 + insert_ten*10 + insert_fifty*50;
+   case(state)
+       IDLE: begin
+           if(clear && cash >= CHANGE_UNIT) next_state = CHANGE;
+           else begin
+               if ( select ) next_state = BUY;
+               else next_state = IDLE;
+           end 
+       end
+       BUY: begin
+           next_state = CHANGE;
+       end
+       CHANGE: begin
+           next_state = (cash == 0) ? IDLE : CHANGE;
+       end
+       default: begin
+           next_state = IDLE;
+       end
+   endcase
 end
 
 
@@ -96,7 +160,7 @@ module OnePulse (
     output reg signal_single_pulse,
     input wire signal,
     input wire clock
-    );
+   );
     
     reg signal_delay;
 
