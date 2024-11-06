a5708e093dc3942f9813adc46c57ebc7bbc3c185
SamTech37 <SamTech37@users.noreply.github.com>
2024-11-06 22:35:57 +0800

    add vending machine 7seg function

--- LAB_5/LAB_5.srcs/modules/Lab5_Team30_Vending_Machine_fpga.v
+++ LAB_5/LAB_5.srcs/modules/Lab5_Team30_Vending_Machine_fpga.v
@@ -1,5 +1,10 @@
 `timescale 1ns/1ps
 
+// [TODO]
+// fix debounce issue
+// add keyboard IO
+// add mechanism for change return
+
 module Vending_Machine_FPGA (
 inout wire PS2_DATA,//use keyboard A,S,D,F for selecting the item
 inout wire PS2_CLK, //one drink at a time. lock the keys? priority?
@@ -31,14 +36,12 @@ OnePulse op2(.clock(clk),.signal(btnC_db),.signal_single_pulse(insert_ten));
 OnePulse op3(.clock(clk),.signal(btnR_db),.signal_single_pulse(insert_fifty));
 OnePulse op4(.clock(clk),.signal(btnD_db),.signal_single_pulse(clear));
 OnePulse op5(.clock(clk),.signal(btnU_db),.signal_single_pulse(reset));
-
-
 assign rst_n = !reset;
 
 
-
-
-Vending_Machine_FSM vendor(clk, reset, clear, insert_five, insert_ten, insert_fifty, outLED);
+wire [8:0] cash;
+Vending_Machine_FSM vendor(clk, reset, clear, insert_five, insert_ten, insert_fifty, outLED, cash);
+SevenSegmentDisplay ssd(.display(display),.digit(digit),.cash(cash),.rst(reset),.clk(clk));
 
 endmodule
 
@@ -49,12 +52,12 @@ module Vending_Machine_FSM (
     input insert_five,
     input insert_ten,
     input insert_fifty,
-    output [3:0]drink_avail
-     
+    output [3:0]drink_avail,
+    output reg [8:0]cash 
 );
 
 
-reg [8:0] cash, next_cash;
+reg [8:0] next_cash;
 parameter MAX = 9'd100;
 parameter MIN = 9'd0;
 parameter WATER = 9'd20;
@@ -89,16 +92,6 @@ end
 endmodule
 
 
-module Clock_Divder (
-    input clk,
-    input rst_n,
-    output clk_d
-);
-
-
-
-endmodule
-
 //Onepulse
 module OnePulse (
     output reg signal_single_pulse,
@@ -124,31 +117,78 @@ module Debounce(
     output pb_debounced
 );
     
-    reg [15:0] DFF;
-    assign pb_debounced = (DFF == 16'hFFFF);
+    reg [64-1:0] DFF;
+    assign pb_debounced = (DFF == {16{4'hF}});
     always @(posedge clk) begin
-        DFF <= {DFF[14:0],pb};
+        DFF <= {DFF[62:0],pb};
     end
 
 endmodule
 
-//concurrent display stuff
-module SevenSegDisplay(
+//concurrently display stuff
+module SevenSegmentDisplay(
     output reg [6:0] display,
-    output reg [3:0] digit,//use the rightmost 3 digits
-    input wire [4:0] num,
-    input wire rst,
+    output reg [3:0] digit,//the rightmost 3 digits
+    input wire [8:0] cash,
+    input wire rst, //active high
     input wire clk
     );
     
-    reg [4:0] display_num;
-    always @ (posedge clk) begin
-        digit <= 4'b1000; 
-        display_num <= num;
+    reg [15:0] clk_divider;
+    reg [3:0] display_num;
+    wire [3:0] nums [2:0]; //3 digits of decimal
+
+    //don't prepend zeros for digit 2 & 3
+    assign nums[0] = cash % 10;//dangerous usage of / and % operator
+    assign nums[1] = (cash < 10)? 4'b1111 : (cash / 10) % 10;
+    assign nums[2] = (cash < 100)? 4'b1111 : cash / 100;
+
+    
+    //counter / clock divider
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+            clk_divider <= 16'b0;
+        end else begin
+            clk_divider <= clk_divider + 16'b1;
+        end
+    end
+    
+    //display cycle 
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+            display_num <= 4'b0000;
+            digit <= 4'b1111;
+        end else if (clk_divider == 16'hFFFF) begin
+            case (digit)
+                4'b1110 : begin
+                    display_num <= nums[1];
+                    digit <= 4'b1101;
+                end
+                4'b1101 : begin
+                    display_num <= nums[2];
+                    digit <= 4'b1011;
+                end
+                // 4'b1011 : begin
+                //     display_num <= nums[15:12];
+                //     digit <= 4'b0111;
+                // end
+                4'b1011 : begin 
+                    display_num <= nums[0];
+                    digit <= 4'b1110;
+                end
+                default : begin
+                    display_num <= nums[0];
+                    digit <= 4'b1110;
+                end				
+            endcase
+        end else begin
+            display_num <= display_num;
+            digit <= digit;
+        end
     end
     
     always @ (*) begin
-        case (display_num)
+        case (display_num) //decode 4-bit number to 7-segment display postion
             0 : display = 7'b1000000;	//0000
             1 : display = 7'b1111001;   //0001                                                
             2 : display = 7'b0100100;   //0010                                                
@@ -159,11 +199,10 @@ module SevenSegDisplay(
             7 : display = 7'b1111000;   //0111
             8 : display = 7'b0000000;   //1000
             9 : display = 7'b0010000;	//1001
-            default : display = 7'b1111111;//all dim
+            default : display = 7'b1111111;
         endcase
     end
     
 endmodule
-
 
 //keyboard I/O
